1079b18109e720f6b9345346b4ce3d6fe0f75798
chikkupa <chikku@hashin.in>
2018-07-09 14:47:09 +0530

    Test commit

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 var Host = "http://localhost"
 var Port = "8080"
 
+/* Database config */
 var Db_name = "go_login_app"
 var Db_user = "root"
 var Db_password = "123"
@@ -11,6 +12,7 @@ var Mysql = "mysql"
 var Dbconnection = Db_user + ":" + Db_password + "@tcp(127.0.0.1:3306)/" + Db_name;
 var BaseUrl = Host + ":" + Port
 
+/* Mail server setup */
 var Mail_smtp_addr = "smtp.gmail.com:587"
 var Mail_smtp_host = "smtp.gmail.com"
 var Mail_username = "chikku.pa@gmail.com"
